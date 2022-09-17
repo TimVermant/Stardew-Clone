ea48d44514602b75cf5a84097da86091060e595a
ajexspeedy <tim.vermant@outlook.com>
2022-09-17 16:46:15 +0200

    Added rotation towards mouse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,18 +10,33 @@ public class PlayerMovement : MonoBehaviour
 
 
     Vector2 m_Movement;
+    Vector3 m_CurrMousePos;
+
+    [SerializeField] private Camera m_Camera;
+
 
     public void MovePlayer(InputAction.CallbackContext context)
     {
         m_Movement = context.ReadValue<Vector2>();
 
-        Debug.Log(m_Movement);
+
+    }
+
+    public void PlayerDirection(InputAction.CallbackContext context)
+    {
+        m_CurrMousePos = m_Camera.ScreenToWorldPoint(context.ReadValue<Vector2>());
 
     }
 
     void FixedUpdate()
     {
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_MovementSpeed * Time.fixedDeltaTime);
-       
+
+
+        // Look at mouse pointer
+        Vector3 rotation = m_CurrMousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        m_Rigidbody.SetRotation(Quaternion.Euler(0f, 0f, rotZ));
+
     }
 }
